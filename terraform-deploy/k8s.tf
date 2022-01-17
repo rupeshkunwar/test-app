84b6c27ae401be9b70838c9139fb7db68745d90b
Rupesh dev <rupeshdev116@gmail.com>
2022-01-17 22:01:03 +0530

    terraform3

--- terraform-deploy/k8s.tf
+++ terraform-deploy/k8s.tf
@@ -1,10 +1,10 @@
 resource "digitalocean_kubernetes_cluster" "multi-k8s" {
-  name   = "${secrets.CLUSTER_NAME}"
+  name   = "${{secrets.CLUSTER_NAME}}"
   region = "${var.REGION}"
   version = "${var.K8S_VERSION}"
 
   node_pool {
-    name       = "${secrets.CLUSTER_NAME}-node"
+    name       = "${{secrets.CLUSTER_NAME}}-node"
     size       = "${var.IMAGE}"
     node_count = 4
 
