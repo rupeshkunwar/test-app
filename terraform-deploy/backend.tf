7000e5b77ffc74adaf7d2e02667755d46d65f47a
Rupesh dev <rupeshdev116@gmail.com>
2022-01-18 00:04:20 +0530

    terraform18

--- terraform-deploy/backend.tf
+++ terraform-deploy/backend.tf
@@ -1,11 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "rupeshaccern-s3-jenkins"
-    key    = "digitalocean/terraform.tfstate"
+    key    = "digitalocean-new/terraform.tfstate"
     region = "ap-south-1"
-    skip_requesting_account_id = true
     skip_credentials_validation = true
-    skip_get_ec2_platforms = true
     skip_metadata_api_check = true
   }
 }
